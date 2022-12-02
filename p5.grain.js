4c8c20d99d2d19d926044f54fc9374132819a074
meezwhite <meezwhite@proton.me>
2022-12-02 02:14:56 +0100

    refactor(version): update version

--- p5.grain.js
+++ p5.grain.js
@@ -1,12 +1,12 @@
 /**!
  * p5.grain
  * 
- * @version 0.4.0
+ * @version 0.5.0
  * @license MIT
  * @copyright meezwhite, Gorilla Sun
  */
 class P5Grain {
-    version = '0.4.0';
+    version = '0.5.0';
 
     /** @internal */
     ignoreWarnings = false;

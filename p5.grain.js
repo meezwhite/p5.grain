26391df39726b303adf20c315741634eee398002
meezwhite <meezwhite@proton.me>
2023-06-10 13:34:17 +0200

    chore: update version

--- p5.grain.js
+++ p5.grain.js
@@ -1,12 +1,12 @@
 /**!
  * p5.grain
  * 
- * @version 0.6.1
+ * @version 0.7.0
  * @license MIT
  * @copyright meezwhite, Gorilla Sun
  */
 class P5Grain {
-    version = '0.6.1';
+    version = '0.7.0';
 
     instance;
     /** @internal */
